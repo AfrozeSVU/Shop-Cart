7e740e7b25fb0d871beb7930f7f2002a7522e164
Afroze <mohammadafroze26@gmail.com>
2024-08-10 10:45:42 +0530

    added header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+ import React from 'react'
+ import  HeaderImage from '../../assets/header_image.avif'
+ import './Header.css'
+ 
+ 
+ const Header = () => {
+   return (
+      <>
+      <div className='header-wrapper'>
+      <h1 className='header-text'>Shopping List</h1>
+      <div className='header-image-wrapper'>
+      <img className='header-image'     src={HeaderImage} alt="" />
+      </div>
+      
+      </div>
+     
+      </>
+   )
+ }
+ 
+ export default Header
