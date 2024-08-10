0f98ea44cae8de596203b9615a9d094307671e44
Afroze <mohammadafroze26@gmail.com>
2024-08-10 17:36:12 +0530

    changed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import ShoppingList from './components/ShoppingList/ShoppingList';
-
+// import ShoppingList from './components/ShoppingList/ShoppingList';
+import ShoppingList from './Components/ShoppingList/ShoppingList';
 
 function App() {
   return (
