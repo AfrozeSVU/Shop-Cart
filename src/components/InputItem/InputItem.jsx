a7f9cdcf529e2601ab3dcb718bc22f14a5b92756
Afroze <mohammadafroze26@gmail.com>
2024-08-10 11:11:16 +0530

    added header

---
+++ src/components/InputItem/InputItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InputItem = () => {
+  return (
+    <div className='item-input-wrapper'> 
+    <input type="text" placeholder='add an item....'/>
+
+    </div>
+  )
+}
+
+export default InputItem
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Header from '../Header/Header'
-
+ import InputItem from '../InputItem/InputItem'
 const ShoppingList = () => {
   return (
     <>  <Header></Header>
+    <div className='currennt-shopping-list'>
+       <InputItem/>
+    </div>
    </>
   )
 }
